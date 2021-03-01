eb41f4aee55a318f059492ba7e31496d9360c66e
gitikakhira69 <79353610+gitikakhira69@users.noreply.github.com>
2021-03-01 17:59:50 +0530

    Add files via upload

---
+++ formdb.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAllfXVhFnpQHAae5LoVwtxiZRAGdK6V2I",
+    authDomain: "corona-testing-data.firebaseapp.com",
+    databaseURL: "https://corona-testing-data-default-rtdb.firebaseio.com",
+    projectId: "corona-testing-data",
+    storageBucket: "corona-testing-data.appspot.com",
+    messagingSenderId: "786270676749",
+    appId: "1:786270676749:web:4ade1503a1526b5ae9b244"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
